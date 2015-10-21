1a37c56b8a2c928d8baf569c19e490ad68f92d43
dargaCode <darga.code@gmail.com>
2015-10-21 18:15:46 -0400

    logic skeleton for reverse

--- reverse.c
+++ reverse.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <cs50.h>
+#include <string.h>
 
 // constants
 #define REQUIRED_ARGS 1
 
 // function prototypes
 bool is_valid_input(int argc, string argv[]);
+string reverse(string str, int len);
 
 int main(int argc, string argv[])
 {
@@ -17,8 +19,13 @@ int main(int argc, string argv[])
     }
     printf("Enter string to reverse: ");
     string input = GetString();
+    int len = strlen(input);
 
     printf("Reverse string \"%s\"\n", input);
+
+    string result = reverse(input, len);
+    printf("New string: %s\n", result);
+
     // success
     return 0;
 }
@@ -30,4 +37,9 @@ bool is_valid_input(int argc, string argv[])
         return false;
     }
     return true;
+}
+
+string reverse(string str, int len)
+{
+    return strcat(str, " TESTING");
 }
