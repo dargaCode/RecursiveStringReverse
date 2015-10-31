161cca64fedb2450fb773de6cc52622f2c94f831
dargaCode <darga.code@gmail.com>
2015-10-31 16:52:47 -0400

    testing pointer increment to shorten string

--- reverse.c
+++ reverse.c
@@ -40,25 +40,14 @@ bool is_valid_input(int argc, string argv[])
 
 char* reverse(char* str)
 {
-    int len = strlen(str);
-    // base case
-    if (len == 1) {
+        for (int i = 0; strlen(str) > 1; i++)
+        {
+            printf("%i, %s (len %i)\n", i, str, strlen(str));
+            str++;
+        }
         return str;
-    }
-    printf("%s \n", str);
-    char* first_char = malloc(2 * sizeof(char));
-    first_char[0] = str[0];
-    first_char[1] = '\0';
-    str++; // ohhhh making a new string is the same as moving
-           // the pointer up.
-    char* result = strcat(reverse(str), first_char);
-    printf("%s \n", result);
-    return result;
 
-    // this will all work easier with putting first at end
-    // instead of putting last at beginning
-    // malloc new string from first char and \0
-    // just move pointer up one for second string
+
 }
 
 
